fd39f130c864e8c8105b95cc40203e64073f7bf0
Kavinesh-sekar <83826820+Kavinesh-sekar@users.noreply.github.com>
2021-06-21 08:02:02 +0530

    Update guessno.js

--- guessno.js
+++ guessno.js
@@ -5,40 +5,53 @@ let timeleft=6;
 button.addEventListener('click',function(e)
 {
 
+console.log(randomno);
     console.log(randomno);
     const inputvalue=document.getElementById('inputno').value;
+     timeleft--;
  
 
   if(isNaN(inputvalue) || inputvalue > 100 || inputvalue <1)
   {
    
      message.innerHTML='<h2 style="color:orange;">Please Enter correct Number Between 1 to 100 </h2>';
-
+     timeleft++;
  
   }
+
 
     else if(inputvalue == randomno)
     {
         message.innerHTML='<h2 style="color:green;">Congulation!! Your Guess was Correct</h2>';
+        button.disabled = true;
+        
     }
     else if(inputvalue<randomno)
     {
        
         message.innerHTML=`<h2 style="color:red;">Too low<br> <br> You have ${timeleft} Remining</h2>`;
-           timeleft -= 1;
+
     }
-    else{
+
+   
+    else if(inputvalue>randomno){
 
         
         message.innerHTML=  `<h2 style="color:red;">Too High<br> <br> You have Remaining ${timeleft} Count </h2>`;
-        timeleft -= 1;
+
     }
 
 if(timeleft == 0)
     {
-        message.innerHTML= `<h2 style="color:red;">Game Over!! Correct no is ${randomno} </h2>`;
-
-        button.disabled = true;
+        if(inputvalue == randomno){
+            message.innerHTML='<h2 style="color:green;">Congulation!! Your Guess was Correct</h2>';
+            button.disabled = true;
+        }
+        else{
+            message.innerHTML= `<h2 style="color:red;">Game Over!! Correct no is ${randomno} </h2>`;
+            button.disabled = true;
+        }
+       
     }
     e.preventDefault();
 
